ac4f26c286c71afe9dc1e0d601359707579335c2
Siri-Ray <2667192167@qq.com>
2026-03-06 14:34:56 +0800

    feat(web,landing): replace placeholder N logo with favicon mark

---
+++ apps/web/src/components/brand-mark.tsx
@@ -0,0 +1,7 @@
+import type { ImgHTMLAttributes } from "react";
+
+type BrandMarkProps = Omit<ImgHTMLAttributes<HTMLImageElement>, "src" | "alt">;
+
+export function BrandMark(props: BrandMarkProps) {
+  return <img src="/favicon/favicon-light.svg" alt="Nexu logo" {...props} />;
+}
